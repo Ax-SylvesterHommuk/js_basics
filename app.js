496af54579a4b72371152dbbce2dd24e5ca9d0f1
ax-sylvester <ax-sylvester.hommuk@khk.ee>
2022-09-20 11:17:33 +0300

    variables

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("hello world!")
+
+// variables
+let name = "Ax-Sylvester Hommuk"
+name = "New name"
+
+// const - ei saa ümber väärtustada
+const surname = "Hommuk"
+// surname = "New surname" - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: 'Ax-Sylvester',
+    surname: 'Hommuk'
+}
+
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = "IKT"
+console.log(person.department)
