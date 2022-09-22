687d4c451f082ae3083de21f718586f376d0a78c
ax-sylvester <ax-sylvester.hommuk@khk.ee>
2022-09-22 13:17:47 +0300

    date processing

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-const person = {
-    firstname: "Kadi",
-    surname: "Tamm",
-    age: 40,
-    email: "kadi.tamm@gmail.com",
-    hobbies: ["tennis", "korvpall"],
-    address: {
-        city: "Võru",
-        county: "Võrumaa"
-    },
-    getBirthYear: function (){
-        return 2022 - this.age
-    }
-}
+const today = new Date()
 
 let val
-val = person
-val = person["firstname"]
-val = person["hobbies"][1]
-val = person.address.city
-val = person.address["city"]
-val = person.getBirthYear()
+
+val = today
+val = today.getMonth()
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getMinutes()
+val = today.getSeconds()
+val = today.getTime()
+
+val = new Date("09/22/2021")
+val = new Date("September 23 2021")
+
+val = new Date()
+val.setDate(23)
+val.setFullYear(2023)
+
+
 console.log(val)
-
-// for (let i = 0; i < person.hobbies.length; i++){
-//     console.log(person.hobbies[i])
-// }
-
-// person.hobbies.forEach(function (hobbie, index, hobbies){
-//     console.log(hobbie + " on element indeksiga " + index)
-//     console.log(hobbies)
-// })
-
-person.hobbies.forEach((hobbie, index) => {
-    console.log(hobbie + " on element indeksiga " + index)
-})
