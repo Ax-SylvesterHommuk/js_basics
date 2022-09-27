962640bac060325a9fcd609020b113e8ed4611d2
ax-sylvester <ax-sylvester.hommuk@khk.ee>
2022-09-27 09:27:22 +0300

    website

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 let val;
 
-val = document
-val = document.all[6]
-val = document.all.length
+// val = document.getElementById("task-form")
+// val = document.getElementById("task-form").id
+// val = document.getElementById("task").className
 
-val= document.head
-val = document.body
+const taskTitle = document.getElementById("task-title")
 
-val = document.doctype
-val = document.domain
-val = document.URL
-val = document.characterSet
-val = document.contentType
+// style
+taskTitle.style.background = '#000000'
+taskTitle.style.color = '#fff'
+taskTitle.style.padding = '5px'
+// taskTitle.style.display = 'none'
 
-val = document.forms
-val = document.forms[0]
-val = document.forms[0].method
-val = document.forms[0].action
-val = document.forms[0].id
+// content
+taskTitle.textContent = 'Defined Tasks'
+taskTitle.textContent = 'My Favorite Tasks'
+taskTitle.innerHTML = '<b style="color: red">My Tasks</b>'
 
-val = document.links
-val = document.links[0]
-val = document.links[0].id
-val = document.links[0].className
-val = document.links[0].classList
-val = document.links[0].classList[2]
+val = document.querySelector('#task-title')
+val = document.querySelector('.card-title')
+val = document.querySelector('ul')
+const task = document.querySelector('li')
+task.style.color = '#ffffff'
 
-val = document.scripts
-val = document.scripts[0]
-val = document.scripts[1]
-val = document.scripts[1].getAttribute("src")
+val = document.querySelector('li:last-child')
+val = document.querySelector('li:nth-child(odd)').style.background = '#ddd'
+val = document.querySelector('li:nth-child(even)').style.background = '#e7e7e7'
+
+// multiple elements
+
+val = document.querySelectorAll('li')
+oddLi = document.querySelectorAll('li:nth-child(odd)')
+
+oddLi.forEach((li) => {
+    li.style.background = '#00ff66'
+})
+
+
+
 console.log(val)
